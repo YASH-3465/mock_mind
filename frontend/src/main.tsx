e4286e1a1d523b6d483faf3c3a33a15d4cea9a64
YASH-3465 <yashgoud3465@gmail.com>
2026-09-13 16:01:59 +0530

    Rebuild Mock Mind frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Toaster } from "sonner";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+    <Toaster position="top-right" richColors />
+  </React.StrictMode>
 );
